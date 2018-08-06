160e3a792e2ba03fa2fdf11f7ba5796cbb88636d
suzuryu <nitirin227@gmail.com>
2018-08-06 14:20:46 +0900

    NANNKA MOU IROIRO KAETA

--- crawring.go
+++ crawring.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -18,12 +19,19 @@ func CheckandLoggingError(err error) {
 	}
 }
 
-func DownloadImgFromURL(img_url string) {
-	const base_url string = "http://animekabegami.com"
-	const excusion_word string = "icon"
+type Anime struct {
+	title string
+	url   string
+}
 
-	// no thanks icon img
-	if strings.Index(img_url, excusion_word) != -1 {
+func DownloadImgFromURL(img_url string, anime Anime) {
+	const base_url string = "http://animekabegami.com"
+
+	// no thanks icon, header img
+	if strings.Index(img_url, "icon") != -1 {
+		return
+	}
+	if strings.Index(img_url, "header") != -1 {
 		return
 	}
 
@@ -41,8 +49,8 @@ func DownloadImgFromURL(img_url string) {
 	defer res.Body.Close()
 
 	img_name := img_url[strings.LastIndex(img_url, "/")+1:]
-	fmt.Println(img_name)
-	file, err := os.Create(img_name)
+	//	fmt.Println(img_name)
+	file, err := os.Create("../../AnimeKabegami/" + anime.title + "/" + img_name)
 	CheckandLoggingError(err)
 	defer file.Close()
 
@@ -50,7 +58,7 @@ func DownloadImgFromURL(img_url string) {
 	io.Copy(file, res.Body)
 }
 
-func GetImgfromWeb(url string) {
+func GetImgfromWeb(url string, anime Anime) {
 	res, err := http.Get(url)
 	CheckandLoggingError(err)
 	defer res.Body.Close()
@@ -63,17 +71,46 @@ func GetImgfromWeb(url string) {
 	CheckandLoggingError(err)
 	doc.Find("img").Each(func(_ int, s *goquery.Selection) {
 		img_url, _ := s.Attr("src")
-		DownloadImgFromURL(img_url)
+		fmt.Println(img_url)
+		time.Sleep(5 * time.Second)
+		DownloadImgFromURL(img_url, anime)
 	})
 
+	doc.Find(".paging .blk2 a").Each(func(_ int, s *goquery.Selection) {
+		if s.Text() == "次へ" {
+			next_url, _ := s.Attr("href")
+			GetImgfromWeb("http://animekabegami.com/"+next_url[1:], anime)
+		}
+	})
+}
+
+func GetAnimeList(url string) {
+	res, err := http.Get(url)
+	CheckandLoggingError(err)
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatal("status code error")
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	CheckandLoggingError(err)
+
+	doc.Find(".side-menu-body ul li a").Each(func(_ int, s *goquery.Selection) {
+		anime := Anime{}
+		anime.url, _ = s.Attr("href")
+		anime.title = s.Text()
+		fmt.Println(anime.title, anime.url)
+		os.Mkdir("../../AnimeKabegami/"+anime.title, 0777)
+		GetImgfromWeb(url+anime.url[2:], anime)
+	})
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("argument error. command line argument must 2")
-		return
-	}
-	anime_title := os.Args[1]
-	url := "http://animekabegami.com/select?title=" + anime_title
-	GetImgfromWeb(url)
+	os.Mkdir("../../AnimeKabegami", 0777)
+	url := "http://animekabegami.com/"
+	//url = "http://animekabegami.com/select?title=C"
+	GetAnimeList(url)
+	//anime := Anime{"C (26)", "http://animekabegami.com/select?title=C"}
+	//GetImgfromWeb(url, anime)
 }
