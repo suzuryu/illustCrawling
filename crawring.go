861436f49014aeceb57899846891071e03199b07
suzuryu <nitirin227@gmail.com>
2018-08-07 09:16:38 +0900

    func func

--- crawring.go
+++ crawring.go
@@ -12,6 +12,19 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type Anime struct {
+	title string
+	url   string
+}
+
+func HttpGet(url string) *http.Response {
+	res, err := http.Get(url)
+	CheckandLoggingError(err)
+	defer res.Body.Close()
+
+	return res
+}
+
 func CheckandLoggingError(err error) {
 	is_err := err != nil
 	if is_err {
@@ -19,24 +32,22 @@ func CheckandLoggingError(err error) {
 	}
 }
 
-type Anime struct {
-	title string
-	url   string
-}
-
-func DownloadImgFromURL(img_url string, anime Anime) {
-	const base_url string = "http://animekabegami.com"
-
+func URLHasExclusionWord(img_url string) bool {
 	// no thanks icon, header img
 	if strings.Index(img_url, "icon") != -1 {
-		return
+		return true
 	}
 	if strings.Index(img_url, "header") != -1 {
-		return
+		return true
 	}
 
+	return false
+}
+
+func RemoveThumb(img_url string) string {
 	// thumb img is mini size image
 	// so i will download original image that has no "thumb" in url
+
 	const thumb_str string = "thumb"
 	if strings.LastIndex(img_url, thumb_str) == -1 {
 		img_url = img_url[1:]
@@ -46,29 +57,48 @@ func DownloadImgFromURL(img_url string, anime Anime) {
 	}
 	fmt.Println(img_url)
 
-	res, err := http.Get(base_url + img_url)
-	CheckandLoggingError(err)
-	defer res.Body.Close()
+	return img_url
+}
 
-	img_name := img_url[strings.LastIndex(img_url, "/")+1:]
-	file, err := os.Create("../../AnimeKabegami/" + anime.title + "/" + img_name)
-	CheckandLoggingError(err)
-	defer file.Close()
+func FileIsExist(file_path string) bool {
+	_, err := os.Stat(file_path)
+	return !os.IsNotExist(err)
+}
+
+func CheckStatusCode(status_code int) {
+	if status_code != 200 {
+		log.Fatal("status code error")
+	}
+}
+
+func DownloadImgFromURL(img_url string, anime Anime) {
+	if URLHasExclusionWord(img_url) {
+		return
+	}
+
+	img_url = RemoveThumb(img_url)
+
+	img_path := "../../AnimeKabegami/" + anime.title + "/" + img_url[strings.LastIndex(img_url, "/")+1:]
+	if FileIsExist(img_path) {
+		return
+	}
+
+	// GET img
+	const base_url string = "http://animekabegami.com"
+	res := HttpGet(base_url + img_url)
 
 	// download image
+	file, err := os.Create(img_path)
+	CheckandLoggingError(err)
+	defer file.Close()
 	io.Copy(file, res.Body)
 
 	time.Sleep(5 * time.Second)
 }
 
 func GetImgfromWeb(url string, anime Anime) {
-	res, err := http.Get(url)
-	CheckandLoggingError(err)
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Fatal("status code error")
-	}
+	res := HttpGet(url)
+	CheckStatusCode(res.StatusCode)
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	CheckandLoggingError(err)
@@ -86,23 +116,22 @@ func GetImgfromWeb(url string, anime Anime) {
 }
 
 func GetAnimeList(url string) {
-	res, err := http.Get(url)
-	CheckandLoggingError(err)
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Fatal("status code error")
-	}
+	res := HttpGet(url)
+	CheckStatusCode(res.StatusCode)
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	CheckandLoggingError(err)
 
 	doc.Find(".side-menu-body ul li a").Each(func(i int, s *goquery.Selection) {
-		if i >= 2 {
+		// すでにダウンロードしてるの飛ばすため１０からスタート
+		if i > 9 {
 			anime := Anime{}
 			anime.url, _ = s.Attr("href")
 			anime.title = s.Text()
 			fmt.Println(anime.title, anime.url)
+			anime.title = strings.Replace(anime.title, "/", "_", -1)
+			anime.title = strings.Replace(anime.title, "?", "_", -1)
+			anime.title = strings.Replace(anime.title, ":", "_", -1)
 			os.Mkdir("../../AnimeKabegami/"+anime.title, 0777)
 			GetImgfromWeb(url+anime.url[2:], anime)
 		}
